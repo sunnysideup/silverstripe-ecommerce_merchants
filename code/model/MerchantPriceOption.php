7722aaa015500fb6032f854d058cc386bf5b7e18
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-03-10 10:23:50 +0000

    ADDING merchant price option
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_merchants/trunk@4055 950b781c-c544-efab-2f99-90083ee69683

---
+++ code/model/MerchantPriceOption.php
@@ -0,0 +1,46 @@
+<?php
+
+class MerchantPriceOption extends DataObject {
+
+	static $db = array(
+		'Price' => 'Currency',
+		'ShowInFrom' => 'Boolean',
+		'ShowInUpTo' => 'Boolean'
+	);
+
+	static $casting = array(
+		'PriceNice' => 'HTMLText',
+		'PriceInt' => 'Int'
+	);
+
+	static $summaryfields = array(
+		'Price' => 'Price'
+	);
+
+	static $sort = "Price ASC";
+
+	static $singular_name = 'Price Option';
+		function i18n_singular_name() {return _t('MerchantPriceOption.SINGULARNAME', self::$singular_name);}
+
+	public static $plural_name = 'Price Options';
+		function i18n_plural_name() {return _t('MerchantPriceOption.PLURALNAME', self::$plural_name);}
+
+	function getPriceNice(){
+		return round($this->Price, 2);
+	}
+
+	function getPriceInt(){
+		return round($this->Price);
+	}
+
+	function Link() {
+		$page = DataObject::get_one('AllMerchantsPage');
+		if($page) {
+			$link = $page->Link()."?";
+			$link .= $page->PriceFromLink($this->Price, false);
+			$link .= $page->PriceUpToLink($this->Price, false);
+			return $link;
+		}
+	}
+
+}
