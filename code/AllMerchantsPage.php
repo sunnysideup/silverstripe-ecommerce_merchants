4cbeb27b59217b55b3048fc925d5cf4835ce631f
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2013-07-08 02:41:33 +0000

    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_merchants/trunk@4561 950b781c-c544-efab-2f99-90083ee69683

--- code/AllMerchantsPage.php
+++ code/AllMerchantsPage.php
@@ -438,6 +438,7 @@ class AllMerchantsPage_Controller extends ProductGroup_Controller {
 		if(Director::is_ajax()) {
 			$variablesForTemplateArray["Products"] = $this->renderWith("ProductsHolder");
 			$variablesForTemplateArray["Form_FilterForm"] = $this->FilterForm()->renderWith("FilterForm");
+			die("bbbbbbbbbbbbbbbbbbbbb");
 			die(print_r($variablesForTemplateArray["Products"], 1));
 			return $variablesForTemplateArray;
 		}
